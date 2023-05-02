4a82a584a43e7ad624f26b310e304ac8a4e6977e
Vladimir <vladsav33@yandex.ru>
2023-05-02 18:30:54 +0300

    Update FilmService.java

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -18,6 +18,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
